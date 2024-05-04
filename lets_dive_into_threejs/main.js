4172087fe7bdd21e7e626f0f5e23b8eede626e4d
hamzatalha77 <hamza_ta7@hotmail.com>
2024-05-05 00:02:30 +0100

    commit

--- lets_dive_into_threejs/main.js
+++ lets_dive_into_threejs/main.js
@@ -10,6 +10,7 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer()
 
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
@@ -21,18 +22,25 @@ const mesh = new THREE.Mesh(boxGeometry, material)
 scene.add(mesh)
 
 camera.position.z = 5
+const planGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide
+})
+const planeMesh = new THREE.Mesh(planGeometry, planeMaterial)
+scene.add(planeMesh)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+
+scene.add(light)
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
+  // mesh.rotation.x += 0.01
+  // mesh.rotation.y += 0.01
+  // planeMesh.rotation.x += 0.01
 }
 
 animate()
-console.log(scene)
-console.log(camera)
-console.log(renderer)
-console.log(boxGeometry)
-console.log(material)
-console.log(mesh)
